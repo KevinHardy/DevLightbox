78bf45fb7459074010f84321ba9fcf89c2f35795
Kevin Hardy <hardy95k@yahoo.com>
2015-02-03 21:18:57 -0700

    update index.html js/app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
 
+$(document).ready(function() {
+	//var $body = $('body');
+	//var $list-items = $('ul').children();//now I can use my created variable instead of calling that big ul.children every single time
 
-
-
-
-
-
-
-
+	//capture click event on image
+	//prevent new page from opening on click
+	$('#imageGallery a').on('click', function(event) {
+		event.preventDefault();
+		console.log('event prevented.')
+	});
+});
