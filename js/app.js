55f0d4a646314b76d4c5ab0ebca79868ebc1f547
Kevin Hardy <hardy95k@yahoo.com>
2015-02-04 20:56:18 -0700

    update css\style.css index.html js\app.js

--- js/app.js
+++ js/app.js
@@ -4,11 +4,33 @@
 $(document).ready(function() {
 	//var $body = $('body');
 	//var $list-items = $('ul').children();//now I can use my created variable instead of calling that big ul.children every single time
+	var $img = $('<img>');
+	var $overlay = $('<div id="overlay"></div>');
+	var $caption = $('<p></p>');
 
+	$('body').append($overlay);
+	$($overlay).append($img);
+	$($overlay).append($caption);
 	//capture click event on image
 	//prevent new page from opening on click
 	$('#imageGallery a').on('click', function(event) {
 		event.preventDefault();
-		console.log('event prevented.')
+		console.log('event prevented.');
+		$($overlay).show();
+
+		var location = $(this).attr('href');
+		console.log(location);
+
+		$img.attr('src', location);
+
+		var captionText = $(this).children().attr('alt');
+		console.log(captionText);
+
+		$caption.text(captionText);
+	});
+
+	//make overlay disapear when click again
+	$($overlay).on('click', function() {
+		$($overlay).hide();
 	});
 });
